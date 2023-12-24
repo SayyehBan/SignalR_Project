719096290b26c27dc35ef9e61e2a0a880f0cce1e
SayyehBan <sdvp1991david@gmail.com>
2023-12-24 17:22:05 +0330

    Add project files.

---
+++ SignalR_Project/Hubs/SiteChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Project.Hubs
+{
+    public class SiteChatHub : Hub
+    {
+        public async Task SendNewMessage(string Sender, string Message)
+        {
+            await Clients.All.SendAsync("getNewMessage", Sender, Message, DateTime.UtcNow.ToShortDateString());
+        }
+
+
+        public override Task OnConnectedAsync()
+        {
+            var s = Context.ConnectionId;
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SignalR_Project/wwwroot/js/SignalRService.js
@@ -0,0 +1,35 @@
+﻿var chatBox = $("#ChatBox");
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .build();
+
+connection.start();
+
+// نمایش چت باکس برای کاربر
+function showChatDialog() {
+    chatBox.css("display", "block");
+}
+
+function Init() {
+    setTimeout(showChatDialog, 1000);
+}
+///ارسال پیام به سرور
+function sendMessage(text) {
+    connection.invoke('SendNewMessage', "بازدید کننده", text);
+}
+///دریافت پیام از سرور
+connection.on("getNewMessage", getMessage);
+function getMessage(sender, message, time) {
+
+    $("#Messages").append("<li><div><span class='name'>" + sender + "</span><span class='time'>" + time +"</span></div><div class='message'>" + message +"</div></li>");
+}
+$(document).ready(function () {
+    Init();
+    // ایجاد وقفه برای ارسال پیام و جلوگیری از رفرش صفحه
+    $("#NewMessageForm").submit(function (e) {
+        e.preventDefault();  // جلوگیری از رفرش فرم
+        var message = $("#MessageInput").val();
+        sendMessage(message);
+        $("#MessageInput").val('');  // پاک کردن محتوای ورودی
+    });
+});
